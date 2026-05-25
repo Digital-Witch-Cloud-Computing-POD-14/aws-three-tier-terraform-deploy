0d1ff6b178627f41f97fe946e8f0261adab988f4
ifeyinwasunshine28-gif <ifeyinwasunshine28@gmail.com>
2026-05-25 21:33:50 +0100

    build infrastructure

--- 01-provider.tf
+++ 01-provider.tf
@@ -1,30 +1,20 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-
 terraform {
   required_providers {
-    godaddy-dns = {
-      source = "veksh/godaddy-dns"
-      version = "0.3.12"
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.0.0-beta2"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.10" # Or any version < 2.11
-    }
-  
   }
 }
 
-provider "godaddy-dns" {
-  api_key    = "better set it in GODADDY_API_KEY"
-  api_secret = "better set it in GODADDY_API_SECRET"
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
+
 provider "aws" {
   region = "us-east-1"
 }
